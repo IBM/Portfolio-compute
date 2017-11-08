8da3c5d6a5a753d7fd78417e49a94f1c9dc9e0f5
Rob Seidman <RobertDSeidman@gmail.com>
2017-11-08 14:55:30 -0500

    Update update.js

--- static/js/update.js
+++ static/js/update.js
@@ -8,6 +8,9 @@ function updateText() {
     var portfolioLists;
     $.get(apiUrl + 'unit_test_portfolios', function(data) {
         $('.enter-portfolio select').html(function() {
+            if(data == "No portfolios found."){
+                return "Please load a portfolio below.";
+            }else{
             var str = '<option value="" disabled="" selected="">[pick portfolio]</option>';
             var parsed = JSON.parse(data)
             console.log("data in get portfolionames" + data);
@@ -17,6 +20,7 @@ function updateText() {
             portfolioLists = parsed;
             console.log("str: " + str)
             return str;
+        }
         });
     });
 }
